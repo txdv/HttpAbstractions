7fff4ef391cb1fdd2ef54bbba4d04a6b7075967d
Andrius Bentkus <andrius.bentkus@gmail.com>
2016-11-30 15:46:52 +0200

    Small typo fix

--- src/Microsoft.AspNetCore.Http.Abstractions/HttpResponse.cs
+++ src/Microsoft.AspNetCore.Http.Abstractions/HttpResponse.cs
@@ -20,7 +20,7 @@ namespace Microsoft.AspNetCore.Http
         };
 
         /// <summary>
-        /// Gets the <see cref="HttpContext"/> for this request.
+        /// Gets the <see cref="HttpContext"/> for this response.
         /// </summary>
         public abstract HttpContext HttpContext { get; }
 
